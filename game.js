cc245b97c2854a55e60465087ef7a6ac393fb8b0
Joelle Perez <49221382+JLionPerez@users.noreply.github.com>
2025-03-11 16:10:00 -0700

    Add playRound function
    Function compares inputs from human and computer
    and determines who wins the round and returns a
    string showcasing the results, such as telling the
    player they have won or lost this round. If player
    won round humanScore gets incremented, and vice versa.

--- game.js
+++ game.js
@@ -1,13 +1,9 @@
-console.log("Hello World")
-
 let getComputerChoice = () => {
     let choice = Math.floor(Math.random() * 3); //Math.floor rounds down values + Math.random returns a value >= 0 or < max"
     if (choice === 0) { return "rock" }
     if (choice === 1) { return "paper" }
     else { return "scissors" }
 }
-
-console.log(getComputerChoice())
 
 let getHumanChoice = () => {
     let choice = prompt("What is your choice? ")
@@ -16,8 +12,56 @@ let getHumanChoice = () => {
     if (choice.toLowerCase() === "scissors") {return "scissors" }
 }
 
-console.log(getHumanChoice())
-
 let humanScore = 0;
 let computerScore = 0;
+
+let playRound = (humanChoice, computerChoice) => {
+    //human chooses rock
+    if ((humanChoice === "rock") && (computerChoice === "rock")) {
+        return "It's a tie!"
+    }
+    if ((humanChoice === "rock") && (computerChoice === "paper")) {
+        computerScore++;
+        return "You lose! Paper beats Rock."
+    }
+    if ((humanChoice === "rock") && (computerChoice === "scissors")) {
+        humanScore++;
+        return "You win! Rock beats Scissors."
+    }
+
+    //human chooses paper
+    if ((humanChoice === "paper") && (computerChoice === "rock")) {
+        humanScore++;
+        return "You win! Paper beats Rock."
+    }
+    if ((humanChoice === "paper") && (computerChoice === "paper")) {
+        return "It's a tie!"
+    }
+    if ((humanChoice === "paper") && (computerChoice === "scissors")) {
+        computerScore++;
+        return "You lose! Scissors beats Paper."
+    }
+
+    //human chooses scissors
+    if ((humanChoice === "scissors") && (computerChoice === "rock")) {
+        computerScore++;
+        return "You lose! Rock beats Scissors."
+    }
+    if ((humanChoice === "scissors") && (computerChoice === "paper")) {
+        humanScore++;
+        return "You win! Scissors beats Paper."
+    }
+    if ((humanChoice === "scissors") && (computerChoice === "scissors")) {
+        return "It's a tie!"
+    }
+}
+
+// playRound testing
+const a = getHumanChoice()
+const b = getComputerChoice()
+console.log(b)
+
+console.log(playRound(a,b))
+
+console.log("Human score: " + humanScore + " Computer Score: " + computerScore)
 
