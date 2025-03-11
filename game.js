fba913e9cd04a0a93b342f9aaa653b703ccc7336
Joelle Perez <49221382+JLionPerez@users.noreply.github.com>
2025-03-11 15:31:35 -0700

    Add game script + link to html
    I forgot to commit for the functions and variables.
    Added getComputerChoice and getHumanChoice as per
    Odin instructions. Used console.log to see functions
    return their intended values. Added score variables
    for both human and computer.

---
+++ game.js
@@ -0,0 +1,23 @@
+console.log("Hello World")
+
+let getComputerChoice = () => {
+    let choice = Math.floor(Math.random() * 3);
+    if (choice === 0) { return "rock" }
+    if (choice === 1) { return "paper" }
+    else { return "scissors" }
+}
+
+console.log(getComputerChoice())
+
+let getHumanChoice = () => {
+    let choice = prompt("What is your choice? ")
+    if (choice.toLowerCase() === "rock" ) { return "rock" }
+    if (choice.toLowerCase() === "paper") { return "paper" }
+    if (choice.toLowerCase() === "scissors") {return "scissors" }
+}
+
+console.log(getHumanChoice())
+
+let humanScore = 0;
+let computerScore = 0;
+
