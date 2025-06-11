ad87fb938450da7924524e9204b11ca8d984eaf6
Joelle Perez <49221382+JLionPerez@users.noreply.github.com>
2025-06-11 02:04:27 -0700

    Add end of round results

--- game.js
+++ game.js
@@ -17,51 +17,52 @@ let playGame = () => {
     let computerScore = 0;
     const playerScore = document.querySelector("#player-score");
     const pcScore = document.querySelector("#pc-score");
+    const results = document.querySelector(".results");
 
     let playRound = (humanChoice, computerChoice) => {
         //human chooses rock
         if ((humanChoice === "rock") && (computerChoice === "rock")) {
-            return "It's a tie!"
+            results.textContent = "It's a tie!";
         }
         if ((humanChoice === "rock") && (computerChoice === "paper")) {
             computerScore++;
             pcScore.textContent = "Computer score: " + computerScore;
-            return "You lose! Paper beats Rock."
+            results.textContent = "You lose! Paper beats Rock."
         }
         if ((humanChoice === "rock") && (computerChoice === "scissors")) {
             humanScore++;
             playerScore.textContent = "Your score: " + humanScore;
-            return "You win! Rock beats Scissors."
+            results.textContent = "You win! Rock beats Scissors."
         }
     
         //human chooses paper
         if ((humanChoice === "paper") && (computerChoice === "rock")) {
             humanScore++;
             playerScore.textContent = "Your score: " + humanScore;
-            return "You win! Paper beats Rock."
+            results.textContent = "You win! Paper beats Rock."
         }
         if ((humanChoice === "paper") && (computerChoice === "paper")) {
-            return "It's a tie!"
+            results.textContent = "It's a tie!"
         }
         if ((humanChoice === "paper") && (computerChoice === "scissors")) {
             computerScore++;
             pcScore.textContent = "Computer score: " + computerScore;
-            return "You lose! Scissors beats Paper."
+            results.textContent = "You lose! Scissors beats Paper."
         }
     
         //human chooses scissors
         if ((humanChoice === "scissors") && (computerChoice === "rock")) {
             computerScore++;
             pcScore.textContent = "Computer score: " + computerScore;
-            return "You lose! Rock beats Scissors."
+            results.textContent = "You lose! Rock beats Scissors."
         }
         if ((humanChoice === "scissors") && (computerChoice === "paper")) {
             humanScore++;
             playerScore.textContent = "Your score: " + humanScore;
-            return "You win! Scissors beats Paper."
+            results.textContent = "You win! Scissors beats Paper."
         }
         if ((humanChoice === "scissors") && (computerChoice === "scissors")) {
-            return "It's a tie!"
+            results.textContent = "It's a tie!"
         }
     }
 
@@ -80,9 +81,6 @@ let playGame = () => {
     scissorsBtn.addEventListener("click", () => {
         console.log(playRound("scissors", getComputerChoice()));
     });
-
-    // final scores
-    // console.log ("Your Score: " + humanScore + " Computer Score: " + computerScore)
 
     if (humanScore > computerScore) { return "You win the game!"}
     if (humanScore === computerScore) { return "The game ends in a tie."}
